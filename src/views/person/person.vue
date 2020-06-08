67e8e5c3e18075956285c730089d934b4c3a5efd
kettoa <18616226261@163.com>
2020-06-08 20:53:24 +0800

    初始化

---
+++ src/views/person/person.vue
@@ -0,0 +1,9 @@
+<!--
+    @author wang.kt
+    @date 2020-06-08 20:53:14
+    @version 1.0
+    @description ____ 功能入口文件
+-->
+<template src="./person.vue.html" lang="html"></template>
+<script src="./person.vue.js" lang="js"></script>
+<style src="./person.vue.scss" scoped lang="scss"></style>
